0e73023751eddb013b32cf7666cd06529b3fd95a
lorinjameson <lwjameson@gmail.com>
2024-05-17 16:08:12 -0400

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import dynamic from './dynamic-astro-config.js';
+
+// We are utilizing a dynamic astro config in this 
+// template.  When the template is used to create 
+// a new project by the AVAnnotate admin application
+// the dynamic configuration will be updated to reflect 
+// the new project
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig(dynamic); // Do not change!
---
+++ dynamic-astro-config.js
@@ -0,0 +1,11 @@
+import react from '@astrojs/react';
+
+// Please note that this file will be replaced by the AVAnnotate Admin
+// application.  No changes made here will be reflected in the deployed 
+// site.
+
+export default {
+  integrations: [react()],
+  site: 'https://AVAnnotate.github.io',
+  base: 'my-repo/dist',
+}
