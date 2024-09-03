ae1058b452ec63c335428c29e3c3fac5b4e8c1f7
Rebecca Black <rebecca.ann.black@gmail.com>
2024-09-03 19:35:09 -0400

    removed unused variables/imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import { dynamicConfig } from './dynamic-astro-config.js';
 
-// We are utilizing a dynamic astro config in this 
-// template.  When the template is used to create 
+// We are utilizing a dynamic astro config in this
+// template.  When the template is used to create
 // a new project by the AVAnnotate admin application
-// the dynamic configuration will be updated to reflect 
+// the dynamic configuration will be updated to reflect
 // the new project
-
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig(dynamicConfig); // Do not change!
