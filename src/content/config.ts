176e87c4987811d58dff13e3b9ced6098cba58bd
Camden Mecklem <c@camdenmecklem.com>
2024-09-04 16:41:16 -0400

    WIP
    convert to collection API and add custom page route

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,24 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
+
+const annotationCollection = defineCollection({
+  type: 'data',
+});
+
+const eventCollection = defineCollection({
+  type: 'data',
+});
 
 const manifestCollection = defineCollection({
   type: 'data',
 });
 
+const pageCollection = defineCollection({
+  type: 'data',
+});
+
 export const collections = {
+  annotations: annotationCollection,
+  events: eventCollection,
   manifests: manifestCollection,
+  pages: pageCollection,
 };
