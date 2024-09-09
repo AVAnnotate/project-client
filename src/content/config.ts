0865be1ed08033f348d69cad5ea003ed6c43b3bb
Camden Mecklem <c@camdenmecklem.com>
2024-09-09 15:33:11 -0400

    add z.any() types to collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,22 +2,27 @@ import { defineCollection, z } from 'astro:content';
 
 const annotationCollection = defineCollection({
   type: 'data',
+  schema: z.any()
 });
 
 const eventCollection = defineCollection({
   type: 'data',
+  schema: z.any()
 });
 
 const manifestCollection = defineCollection({
   type: 'data',
+  schema: z.any()
 });
 
 const pageCollection = defineCollection({
   type: 'data',
+  schema: z.any()
 });
 
 const projectCollection = defineCollection({
   type: 'data',
+  schema: z.any()
 });
 
 export const collections = {
