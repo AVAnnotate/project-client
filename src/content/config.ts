a7943455de5d2a1a61b5310922ad0d68e01c58a2
Camden <c@camdenmecklem.com>
2024-09-09 15:07:18 -0400

    Initial version of custom pages and migration to Astro content API (#8)
    * WIP
    
    convert to collection API and add custom page route
    
    * migrate latest code
    
    * add project collection to config
    
    * finish content collections migration
    
    * remove @data path from tsconfig

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,29 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
+
+const annotationCollection = defineCollection({
+  type: 'data',
+});
+
+const eventCollection = defineCollection({
+  type: 'data',
+});
 
 const manifestCollection = defineCollection({
   type: 'data',
 });
 
+const pageCollection = defineCollection({
+  type: 'data',
+});
+
+const projectCollection = defineCollection({
+  type: 'data',
+});
+
 export const collections = {
+  annotations: annotationCollection,
+  events: eventCollection,
   manifests: manifestCollection,
+  pages: pageCollection,
+  project: projectCollection,
 };
