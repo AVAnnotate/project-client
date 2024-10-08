f797ddb75e2099ebbc044e49ca80eecbe2e0c19f
Camden Mecklem <c@camdenmecklem.com>
2024-10-08 11:26:29 -0400

    WIP

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import { Bars3Icon } from '@heroicons/react/24/outline';
+
+const Sidebar: React.FC = () => {
+  return (
+    <>
+      <Bars3Icon className='w-8 h-8' />
+    </>
+  );
+};
+
+export default Sidebar;
