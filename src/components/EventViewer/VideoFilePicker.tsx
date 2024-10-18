aefabdfe461675e02642d600639932af90b29dee
Camden Mecklem <c@camdenmecklem.com>
2024-10-18 15:18:31 -0400

    initial version

--- src/components/EventViewer/VideoFilePicker.tsx
+++ src/components/EventViewer/VideoFilePicker.tsx
@@ -1,12 +1,57 @@
 import { useStore } from '@nanostores/react';
 import type { CollectionEntry } from 'astro:content';
-import { useState } from 'react';
-import { PlayFill } from 'react-bootstrap-icons';
+import { useEffect, useRef, useState } from 'react';
 import { $pagePlayersState, setAvFile } from 'src/store.ts';
 import { formatTimestamp } from 'src/utils/player.ts';
-import RichText from '@components/RichText/index.astro';
 
-interface Props {
+interface ThumbCanvasProps {
+  url: string;
+}
+
+const ThumbCanvas: React.FC<ThumbCanvasProps> = (props) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const videoEl = document.createElement('video');
+      videoEl.src = props.url;
+
+      videoEl.onloadeddata = () => {
+        // set the internal canvas dimensions to 2x for better image quality
+        // this doesn't affect CSS, just the canvas's own drawing surface
+        canvasRef.current.height = 180;
+        canvasRef.current.width = 240;
+        const ctx = canvasRef.current.getContext('2d', { alpha: false });
+
+        // draw black background
+        ctx.beginPath();
+        ctx.fillStyle = '#000000';
+        ctx.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+
+        const videoRatio = videoEl.videoHeight / videoEl.videoWidth;
+        const canvasRatio = canvasRef.current.height / canvasRef.current.width;
+
+        if (videoRatio > canvasRatio) {
+          const height = canvasRef.current.width * videoRatio;
+          ctx.drawImage(
+            videoEl,
+            0,
+            Math.floor(canvasRef.current.height - height / 2),
+            canvasRef.current.width,
+            height
+          );
+        } else {
+          const width = (canvasRef.current.width * canvasRatio) / videoRatio;
+          ctx.drawImage(videoEl, 0, 0, width, canvasRef.current.height);
+        }
+      };
+    }
+  }, [props.url, canvasRef.current]);
+
+  return <canvas className='w-[120px] h-[90px]' ref={canvasRef} />;
+};
+
+interface VideoFilePickerProps {
   event: CollectionEntry<'events'>;
   playerId: string;
   // "children" is the rich text component for the description.
@@ -16,19 +61,18 @@ interface Props {
   children?: any;
 }
 
-const VideoFilePicker: React.FC<Props> = (props) => {
+const VideoFilePicker: React.FC<VideoFilePickerProps> = (props) => {
   const [tab, setTab] = useState<'desc' | 'picker'>(
     props.children ? 'desc' : 'picker'
   );
-  const store = useStore($pagePlayersState);
 
   const getButtonClassNames = (buttonTab: typeof tab) => {
-    const baseStyle = '';
+    const baseStyle = 'py-2 mb-4';
 
     if (buttonTab === tab) {
-      return `${baseStyle} underline`;
+      return `${baseStyle} border-b-2 border-solid border-secondary mt-[2px]`;
     } else {
-      return `${baseStyle} underline text-gray-400`;
+      return `${baseStyle} text-gray-400`;
     }
   };
 
@@ -51,40 +95,35 @@ const VideoFilePicker: React.FC<Props> = (props) => {
         </button>
       </div>
       <div>
-        {tab === 'desc' && (
-          <>
-            {props.children}
-            {props.event.data.citation && <p>{props.event.data.citation}</p>}
-          </>
-        )}
-        {tab === 'picker' && (
-          <ol className='flex gap-2 flex-wrap'>
-            {Object.keys(props.event.data.audiovisual_files).map(
-              (uuid, idx) => {
-                const avFile = props.event.data.audiovisual_files[uuid];
-                const isCurrentFile =
-                  store[props.playerId]?.avFileUuid === uuid;
+        <div className={tab !== 'desc' ? 'hidden' : ''}>
+          {props.children}
+          {props.event.data.citation && <p>{props.event.data.citation}</p>}
+        </div>
+        <div
+          className={`grid grid-cols-2 gap-2 py-2 ${
+            tab !== 'picker' ? 'hidden' : ''
+          }`}
+        >
+          {Object.keys(props.event.data.audiovisual_files).map((uuid, idx) => {
+            const avFile = props.event.data.audiovisual_files[uuid];
 
-                return (
-                  <li
-                    className='flex gap-2 items-center hover:cursor-pointer'
-                    onClick={() => setAvFile(uuid, props.playerId)}
-                    key={uuid}
-                  >
-                    <div className='w-4'>{isCurrentFile && <PlayFill />}</div>
-                    <span className={isCurrentFile ? 'font-bold' : ''}>
-                      {idx + 1}. &nbsp;
-                      {avFile.label}
-                    </span>
-                    <span className='flex items-center border border-black rounded-[5px] text-xs p-2 font-semibold h-6'>
-                      {formatTimestamp(avFile.duration, false)}
-                    </span>
-                  </li>
-                );
-              }
-            )}
-          </ol>
-        )}
+            return (
+              <div
+                className='h-[90px] hover:cursor-pointer hover:bg-gray-200 flex gap-2'
+                key={uuid}
+                onClick={() => setAvFile(uuid, props.playerId)}
+              >
+                <ThumbCanvas url={avFile.file_url} />
+                <div className='flex flex-col gap-2'>
+                  <p>
+                    {idx + 1}.&nbsp;{avFile.label}
+                  </p>
+                  <p>{formatTimestamp(avFile.duration, false)}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
